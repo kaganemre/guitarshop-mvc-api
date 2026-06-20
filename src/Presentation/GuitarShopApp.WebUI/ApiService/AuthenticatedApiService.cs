4d5731f54691a8e4b1dab1804befae809af71511
Emre Kağan Erde <emrekaganerde@outlook.com>
2026-06-20 18:32:15 +0300

    Add AuthenticatedApiService to manage token-protected API requests

---
+++ src/Presentation/GuitarShopApp.WebUI/ApiService/AuthenticatedApiService.cs
@@ -0,0 +1,24 @@
+namespace GuitarShopApp.WebUI.ApiService;
+
+public abstract class AuthenticatedApiService<T> : GenericApiService<T> where T : class
+{
+    protected AuthenticatedApiService(HttpClient httpClient) : base(httpClient) { }
+
+    public virtual async Task CreateAsync(T model)
+    {
+        await _httpClient.AddTokenToHeader();
+        await _httpClient.PostAsync("", Serialize(model));
+    }
+
+    public virtual async Task UpdateAsync(T model)
+    {
+        await _httpClient.AddTokenToHeader();
+        await _httpClient.PutAsync("", Serialize(model));
+    }
+
+    public virtual async Task DeleteAsync(int id)
+    {
+        await _httpClient.AddTokenToHeader();
+        await _httpClient.DeleteAsync($"{id}");
+    }
+}
