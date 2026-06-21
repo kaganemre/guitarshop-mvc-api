f05a4dcd70f4924e5bc6b7b67e9e835db0c5f481
Emre Kağan Erde <emrekaganerde@outlook.com>
2026-06-21 10:10:14 +0300

    implement ProductApiService and CategoryApiService

--- src/Presentation/GuitarShopApp.WebUI/ApiService/CategoryApiService.cs
+++ src/Presentation/GuitarShopApp.WebUI/ApiService/CategoryApiService.cs
@@ -1,25 +1,8 @@
-using System.Text.Json;
 using GuitarShopApp.Application.DTO;
 
 namespace GuitarShopApp.WebUI.ApiService;
 
-public class CategoryApiService
+public sealed class CategoryApiService : GenericApiService<CategoryDTO>
 {
-    private readonly HttpClient _httpClient;
-    private readonly JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
-    public CategoryApiService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
-
-    public async Task<IEnumerable<CategoryDTO>> GetAll()
-    {
-        var response = await _httpClient.GetAsync("");
-        string apiResponse = await response.Content.ReadAsStringAsync();
-        var products = JsonSerializer.Deserialize<IEnumerable<CategoryDTO>>(apiResponse, options);
-
-        return products ?? [];
-    }
-
-
+    public CategoryApiService(HttpClient httpClient) : base(httpClient) { }
 }
--- src/Presentation/GuitarShopApp.WebUI/ApiService/ProductApiService.cs
+++ src/Presentation/GuitarShopApp.WebUI/ApiService/ProductApiService.cs
@@ -1,60 +1,19 @@
-using System.Text;
 using System.Text.Json;
 using GuitarShopApp.Application.DTO;
 
 namespace GuitarShopApp.WebUI.ApiService;
 
-public class ProductApiService
+public sealed class ProductApiService : AuthenticatedApiService<ProductDTO>
 {
-    private readonly HttpClient _httpClient;
-
-    // PropertyName compatibility is set when deserializing from JSON object to C# object.
-    // You don't need to use JsonPropertyName attribute on every field.
-    private readonly JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
-    public ProductApiService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
-    public async Task<IEnumerable<ProductDTO>> GetAll()
-    {
-        var response = await _httpClient.GetAsync("");
-        string apiResponse = await response.Content.ReadAsStringAsync();
-        var products = JsonSerializer.Deserialize<IEnumerable<ProductDTO>>(apiResponse, options);
-
-        return products ?? [];
-    }
-    public async Task<ProductDTO> GetById(int? id)
-    {
-        var response = await _httpClient.GetAsync($"{id}");
-        string apiResponse = await response.Content.ReadAsStringAsync();
-        var product = JsonSerializer.Deserialize<ProductDTO>(apiResponse, options);
-
-        return product ?? new ProductDTO();
-    }
+    public ProductApiService(HttpClient httpClient) : base(httpClient) { }
     public async Task<IEnumerable<ProductDTO>> GetProductsByCategory(string category)
     {
         var response = await _httpClient.GetAsync($"get-by-category/{category}");
-
-        string apiResponse = await response.Content.ReadAsStringAsync();
-        var products = JsonSerializer.Deserialize<IEnumerable<ProductDTO>>(apiResponse, options);
+        string json = await response.Content.ReadAsStringAsync();
+        var products = JsonSerializer.Deserialize<IEnumerable<ProductDTO>>(json, _jsonOptions);
 
         return products ?? new List<ProductDTO>();
     }
-    public async Task CreateAsync(ProductDTO model)
-    {
-        await _httpClient.AddTokenToHeader();
-        var createContent = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
-        await _httpClient.PostAsync("", createContent);
-    }
-    public async Task UpdateAsync(ProductDTO model)
-    {
-        await _httpClient.AddTokenToHeader();
-        var updateContent = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
-        await _httpClient.PutAsync("", updateContent);
-    }
-    public async Task DeleteAsync(ProductDTO model)
-    {
-        await _httpClient.AddTokenToHeader();
-        await _httpClient.DeleteAsync($"{model.Id}");
-    }
+
+    public async Task DeleteAsync(ProductDTO model) => await DeleteAsync(model.Id);
 }
